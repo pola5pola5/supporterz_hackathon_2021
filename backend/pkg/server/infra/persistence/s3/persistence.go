f86ab6fce3778cb6c553be267ec423ab9443da78
littletake <ritorut18@gmail.com>
2021-07-02 11:19:42 +0900

    fix: #26

--- backend/pkg/server/infra/persistence/s3/persistence.go
+++ backend/pkg/server/infra/persistence/s3/persistence.go
@@ -2,7 +2,6 @@ package s3
 
 import (
 	"bytes"
-	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -40,7 +39,6 @@ func (sp s3Persistence) SaveFile(filename string, file []byte) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	fmt.Println(output.Location)
 	return output.Location, nil
 }
 
