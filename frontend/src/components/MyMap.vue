1f9d04cdb0d4bd060aaadc2b86d96ad40c93ccf4
tkumakiri <ogdrenyz0117@gmail.com>
2021-07-13 10:08:23 +0900

    fix:#33 - 写真一枚でも表示可能

--- frontend/src/components/MyMap.vue
+++ frontend/src/components/MyMap.vue
@@ -47,6 +47,11 @@ export default {
         if (idx < array.length - 1) {
           jsonCoordinates = jsonCoordinates + ";";
         }
+        if(idx === array.length - 1){
+          jsonCoordinates = jsonCoordinates + ";" 
+          + jsonData.geometry.coordinates[0] + "," 
+          + jsonData.geometry.coordinates[1];
+        }
       });
       await axios
         .get(
@@ -69,7 +74,6 @@ export default {
         "pk.eyJ1IjoidHBrdW1hIiwiYSI6ImNrb3gzbGE5aDBhZ2cyd28xb3R5cG1jZXIifQ.jI7aje2MHl9teidoNmYDPA";
       const map = new mapboxgl.Map({
         container: "map",
-        //style: "mapbox://styles/mapbox/streets-v11",
         style: "mapbox://styles/tpkuma/ckr1c20cv1c4f18qcbsrr2gmm",
         center: route[0],
         zoom: 15,
