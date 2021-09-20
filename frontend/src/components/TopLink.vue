582c789f23414c7b298ddb702273da7f77a01a76
pola5pola5 <l.c.a.m.ayn@gmail.com>
2021-09-20 14:07:57 +0900

    feat: #66 - TOP画面の編集

--- frontend/src/components/TopLink.vue
+++ frontend/src/components/TopLink.vue
@@ -1,35 +1,7 @@
 <template>
   <div class="TopLink">
-    <br />
-    <b>{{ msg }}</b>
-    <br />
-
-    <!-- <router-link to='/input'>
-      <h3 class="to_input">inputに遷移</h3>
-    </router-link>
-    <router-link to='/home'>
-      <h3 class="to_home">homeに遷移</h3>
-    </router-link> -->
-    <router-link to="/signin">
-      <h3 class="to_signin">ログイン</h3>
-    </router-link>
-    <router-link to="/signup">
-      <h3 class="to_signup">初めての方はこちら</h3>
-    </router-link>
-
-    <!-- <h4>チェック用リンク</h4>
-    <ul>
-      <li>
-        <a href="/getpost" target="_blank" rel="noopener">GetPost</a>
-      </li>
-      <li>
-        <a href="/upload" target="_blank" rel="noopener">upload</a>
-      </li>
-      <li>
-        <a href="/input" target="_blank" rel="noopener">input</a
-        >
-      </li>
-    </ul> -->
+    <h2>{{ msg }}</h2>
+    <a href="/signup">Try now</a>
   </div>
 </template>
 
@@ -44,80 +16,35 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1 {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  margin: 40px 0 0;
-  color: #42b983;
+* {
+  box-sizing: border-box;
 }
-.to_input {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  position: relative;
-  color: white;
-  background-color: #42b983;
-  width: 150px;
-  height: 30px;
-  border-radius: 8px;
 
-  margin-left: auto;
-  margin-right: auto;
+h2{
+  margin-top: 20px;
+  margin-bottom: 50px;
+  font-size: 40px;
+  font-weight: normal;
+  color: #ffffff;
+  letter-spacing : 0.2em;
+  text-align: center;
 }
-.to_home {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  position: relative;
-  color: white;
-  background-color: #42b983;
-  width: 150px;
-  height: 30px;
-  border-radius: 8px;
+.TopLink{
+  background: #2d2d2d top center / cover no-repeat;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  font-family: serif;
+}
 
-  margin-left: auto;
-  margin-right: auto;
-}
-.to_signin {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  position: relative;
-  color: white;
-  font-size: 30px;
-  background-color: #42b983;
-  width: 150px;
-  height: 42px;
-  border-radius: 8px;
-
-  margin-left: auto;
-  margin-right: auto;
-}
-.to_signup {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  position: relative;
-  color: white;
-  font-size: 15px;
-  background-color: #42b983;
-  width: 180px;
-  height: 24px;
-  border-radius: 8px;
-
-  margin-left: auto;
-  margin-right: auto;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 a {
-  color: #42b983;
+  margin-top: 20px;
+  margin-bottom: 50px;
+  text-decoration: none;
+  font-size: 15px;
+  color: #ffffff;
+  cursor: pointer;
+  border: 0px;
 }
 </style>
--- frontend/src/views/Top.vue
+++ frontend/src/views/Top.vue
@@ -1,16 +1,37 @@
 <template>
   <div class="top">
-    <br />
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <header class="header">
+      <h1><a href="/">
+        フォト旅
+      </a></h1>
+      <!--      <router-link to="/signin" tag="button" class="signin_btn">Sign in</router-link>-->
+      <!--      <router-link to="/signup" tag="button" class="signup_btn">Sign up</router-link>-->
+      <nav class="pc_nav">
+        <ul>
+          <li><a href="/signin">Sign In</a></li>
+          <li><a href="/signup">Sign Up</a></li>
+        </ul>
+      </nav>
+<!--      <nav class="sp_nav">-->
+<!--        <ul>-->
+<!--          <li><a href="/signin">Sign In</a></li>-->
+<!--          <li><a href="/signup">Sign Up</a></li>-->
+<!--          <li class="close"><span>閉じる</span></li>-->
+<!--        </ul>-->
+<!--      </nav>-->
+      <div id="hamburger">
+        <span></span>
+      </div>
+    </header>
+    <TopLink class="toplink" msg="写真で巡るあなたの旅物語" />
+    <main class="main">main</main>
 
-    <TopLink msg="写真の位置情報からマップを作成します．" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import TopLink from "@/components/TopLink.vue";
-
 export default {
   name: "Top",
   components: {
@@ -19,11 +40,157 @@ export default {
 };
 </script>
 <style scoped>
-/* p {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  margin: 40px 0 0;
-  color: #42b983;
-} */
+* {
+  box-sizing: border-box;
+}
+.top{
+  display: grid;
+   grid-template-rows: 60px 300px 1fr
+}
+.header{
+  grid-row-start: 1;
+  grid-row-end: auto;
+  font-family: serif;
+  padding: 30px 4% 10px;
+  background: #2d2d2d;
+  /*background-color: transparent;*/
+  /*親要素の背景になるけどうまくできてない*/
+  display: flex;
+  align-items: center;
+}
+h1 {
+  margin: 0; padding: 0;
+  font-size: 20px;
+}
+a {
+  text-decoration: none;
+  color: #ffffff;
+  cursor: pointer;
+}
+nav {
+  margin: 0 0 0 auto;
+}
+ul {
+  list-style: none;
+  margin: 0;
+  display: flex;
+}
+li {
+  margin: 0 0 0 15px;
+  font-size: 14px;
+}
+.toplink {
+  grid-row-start: 2;
+  grid-row-end: auto;
+}
+.main{
+  grid-row-start: 3;
+  grid-row-end: auto;
+}
+@media screen and (max-width: 640px) {
+  .pc_nav {
+    display: none;
+  }
+}
+#hamburger {
+  position: relative;
+  display: block;
+  width: 30px;
+  height: 25px;
+  margin: 0 0 0 auto;
+}
+/*#hamburger span {*/
+/*  position: absolute;*/
+/*  top: 50%;*/
+/*  left: 0;*/
+/*  display: block;*/
+/*  width: 100%;*/
+/*  height: 2px;*/
+/*  background-color: #fff;*/
+/*  transform: translateY(-50%);*/
+/*}*/
+/*#hamburger::before {*/
+/*  content: '';*/
+/*  display: block;*/
+/*  position: absolute;*/
+/*  top: 0;*/
+/*  left: 0;*/
+/*  width: 100%;*/
+/*  height: 2px;*/
+/*  background-color: #fff;*/
+/*}*/
+/*#hamburger::after {*/
+/*  content: '';*/
+/*  display: block;*/
+/*  position: absolute;*/
+/*  bottom: 0;*/
+/*  left: 0;*/
+/*  width: 70%;*/
+/*  height: 2px;*/
+/*  background-color: #fff;*/
+/*}*/
+/*.sp_nav {*/
+/*  z-index: 1;*/
+/*  position: fixed;*/
+/*  top: 0;*/
+/*  left: 0;*/
+/*  width: 100%;*/
+/*  height: 100vh;*/
+/*  display: block;*/
+/*  width: 100%;*/
+/*  background: rgba(0, 0, 0, .8);*/
+/*  transition: all .2s ease-in-out;*/
+/*  opacity: 0;*/
+/*  transform: translateY(-100%);*/
+/*}*/
+/*.sp_nav ul {*/
+/*  padding: 0;*/
+/*  display: flex;*/
+/*  flex-direction: column;*/
+/*  justify-content: center;*/
+/*  align-items: center;*/
+/*  height: 100%;*/
+/*}*/
+/*.sp_nav li {*/
+/*  margin: 0;*/
+/*  padding: 0;*/
+/*}*/
+/*.sp_nav li span {*/
+/*  font-size: 15px;*/
+/*  color: #fff;*/
+/*}*/
+/*.sp_nav li a, .sp_nav li span {*/
+/*  display: block;*/
+/*  padding: 20px 0;*/
+/*}*/
+/*!*基準となるli要素*!*/
+/*.sp_nav .close {*/
+/*  position: relative;*/
+/*  padding-left: 20px;*/
+/*}*/
+/*!*バツ印線1*!*/
+/*.sp_nav .close::before {*/
+/*  content: '';*/
+/*  position: absolute;*/
+/*  top: 50%;*/
+/*  left: 0;*/
+/*  display: block;*/
+/*  width: 16px;*/
+/*  height: 1px;*/
+/*  background: #fff;*/
+/*  transform: rotate( 45deg );*/
+/*}*/
+/*!*バツ印線2*!*/
+/*.sp_nav .close::after {*/
+/*  content: '';*/
+/*  position: absolute;*/
+/*  top: 50%;*/
+/*  left: 0;*/
+/*  display: block;*/
+/*  width: 16px;*/
+/*  height: 1px;*/
+/*  background: #fff;*/
+/*  transform: rotate( -45deg );*/
+/*}*/
+
 </style>
